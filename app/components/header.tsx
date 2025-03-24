d2526bea0d26a671463991f7b79bf0ac98eae4ef
Iván Hernández Alvarez <iha205proyecto@gmail.com>
2025-03-24 10:07:44 +0100

    arreglar enlace de header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
           className={`${geistSans.variable} ${geistMono.variable} bg-gray-100 p-4 shadow-md fixed top-0 w-full z-10 left-0 right-0`}
       >
           <div className="container mx-auto flex justify-between items-center">
-              <Link href="/apuntes" className="text-2xl font-bold text-gray-800 hover:text-blue-600 transition-colors">
+              <Link href="/" className="text-2xl font-bold text-gray-800 hover:text-blue-600 transition-colors">
                   ApuntesTech
               </Link>
               <nav>
