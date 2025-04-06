839682f12534ae95ce30e19c1f6fa58433e92fc5
Iván Hernández Alvarez <iha205proyecto@gmail.com>
2025-04-06 11:13:25 +0200

    añadir comentarios a las apis

--- app/api/pdfs/route.ts
+++ app/api/pdfs/route.ts
@@ -1,12 +1,19 @@
 import { NextResponse } from 'next/server';
 import { list, ListBlobResult } from '@vercel/blob';
 
+/**
+ * Este endpoint obtiene y devuelve una lista de archivos almacenados en Vercel Blob.
+ */
 export async function GET(): Promise<NextResponse<ListBlobResult | { message: string }>> {
-    try {
-        const blobs = await list();
-        return NextResponse.json(blobs);
-    } catch (error) {
-        console.error('Error al listar los blobs:', error);
-        return NextResponse.json({ message: 'Error al listar blobs' });
-    }
+  try {
+    // Obtiene la lista de archivos (blobs) desde Vercel Blob.
+    const blobs = await list();
+    // Devuelve la lista de blobs como una respuesta JSON.
+    return NextResponse.json(blobs);
+  } catch (error) {
+    // Si ocurre un error, lo registra en la consola.
+    console.error('Error al obtener la lista de archivos:', error);
+    // Devuelve un mensaje de error al cliente.
+    return NextResponse.json({ message: 'Error al obtener la lista de archivos' });
+  }
 }
